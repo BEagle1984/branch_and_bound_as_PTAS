7b2a2a91452ab20bf5ef75f9ef18b807560fcd18
ztiziano <tiziano.zamaroni@gmail.com>
2025-10-15 17:37:04 +0200

    Implement greedy algorithm (#2)
    * Implement greedy algorithm
    
    * simplified the greedy strategy to compute the lower bound
    
    ---------
    
    Co-authored-by: BEagle1984 <sergio@aquilini.ch>

--- bounds/identical_job_scheduling.py
+++ bounds/identical_job_scheduling.py
@@ -1,5 +1,68 @@
+from collections import deque
 from pyscipopt import Model, SCIP_PARAMSETTING
 from utils import is_integer_val
+
+
+def solve_greedy(n_jobs: int, n_machines: int, processing_times: list[int], overhead: list[float], fixed_jobs: list[tuple[int,int]], verbose: bool = False):
+    """
+        Parameters
+        ----------
+        n_jobs : int
+            Number of jobs to be scheduled.
+        n_machines : int
+            Number of identical machines available.
+        processing_times : list
+            List of processing times for each job.
+        overhead : list
+            List with all completion times, one for each machine
+        fixed_jobs : list
+            List of tuples with the fixed items, (j, i) --> item j is fixed on machine i
+
+        Returns
+        -------
+        solution : dict
+            Dictionary with the solution, (j, i): f means job j is assigned to machine i with fraction f
+        makespan : float
+            The makespan of the solution
+        is_feasible : bool
+            True if the solution is feasible, False otherwise
+    """
+    # Pre-conditions
+    assert sum(overhead) == sum(processing_times[j] for j, i in fixed_jobs), f"The overhead does not match the fixed jobs: {sum(overhead)} != {sum(processing_times[j] for j, i in fixed_jobs)}"
+
+    # Initialize the completion times for each machine
+    completion_times = overhead.copy()
+
+    # Compute the optimal makespan (C_max)
+    C_max = sum(processing_times) / n_machines
+
+    # List of unfixed jobs
+    unfixed_jobs = [j for j in range(n_jobs) if j not in [j for j, i in fixed_jobs]]
+
+    # Assign unfixed jobs to machines using a greedy approach
+    solution = {}
+    machines = deque(range(n_machines))
+    for j in unfixed_jobs:
+        p_j = processing_times[j]
+        p_j_remaining = p_j
+        
+        while machines and p_j_remaining > 0:
+            i = machines[0]
+            if completion_times[i] < C_max:
+                free_processing_time = C_max - completion_times[i]
+                assign_processing_time = min(p_j_remaining, free_processing_time)
+                # Assign job j to machine i
+                solution[j, i] = assign_processing_time / p_j
+                completion_times[i] += assign_processing_time
+                p_j_remaining -= assign_processing_time
+            else:
+                # Machine i exceeds C_max, don't use it
+                machines.popleft()
+    
+    # Post-conditions
+    assert abs(sum(solution[(j, i)] for (j, i) in solution.keys())-len(unfixed_jobs)) < 1e-9, "Some unfixed jobs have not been completely assigned, or are over-assigned."
+
+    return solution, max(completion_times), True
 
 
 def binary_search(n_jobs: int, n_machines: int, processing_times: list[int], overhead, fixed, verbose=False):
@@ -37,8 +100,21 @@ def binary_search(n_jobs: int, n_machines: int, processing_times: list[int], ove
     while right - left > 1:
         middle = (left + right) // 2
         model = Model("Unrelated Job Scheduling")
+        model.setParam("lp/resolvealgorithm", 'p')
+        model.setParam('lp/initalgorithm', 'p')  # Use primal simplex for the initial LP
+        model.setParam('lp/resolvealgorithm', 'p')  # Use primal simplex for re-solves
+
+        # Configure simple Branch-and-Bound
+        model.setIntParam("presolving/maxrounds", 0)  # Disable presolve
+        model.setIntParam("separating/maxrounds", 0)  # Disable cutting planes
+        model.setHeuristics(SCIP_PARAMSETTING.OFF)  # Disable heuristics
+        model.setIntParam("presolving/maxrestarts", 0)  # Disable restarts
+        model.setIntParam("propagating/maxrounds", 0)  # Disable propagation
+
         if not verbose:
             model.hideOutput()
+        else:
+            model.setParam('display/verblevel', 5)
 
         # Decision variables
         x = {}
@@ -57,15 +133,7 @@ def binary_search(n_jobs: int, n_machines: int, processing_times: list[int], ove
         for i in range(n_machines):
             model.addCons(sum(x[j, i] * processing_times[j] for j in unfixed_jobs) <= middle - overhead[i])
 
-        # Configure simple Branch-and-Bound
-        model.setIntParam("presolving/maxrounds", 0)  # Disable presolve
-        model.setIntParam("separating/maxrounds", 0)  # Disable cutting planes
-        model.setHeuristics(SCIP_PARAMSETTING.OFF)  # Disable heuristics
-        model.setIntParam("presolving/maxrestarts", 0)  # Disable restarts
-        model.setIntParam("propagating/maxrounds", 0)  # Disable propagation
-        model.setParam("lp/resolvealgorithm", 'p')
-
-        # model.setObjective(sum(x[0, i] for i in range(n_machines)), "minimize")
+        model.setObjective(x[unfixed_jobs[0], 0], "minimize")
 
         # Optimize the model
         model.optimize()
@@ -74,7 +142,7 @@ def binary_search(n_jobs: int, n_machines: int, processing_times: list[int], ove
         if model.getStatus() == "optimal":
             solution = {(j, i): model.getVal(x[(j, i)]) for j in unfixed_jobs for i in
                         range(n_machines) if model.getVal(x[(j, i)]) > 0}
-            # assert len(list(set([j for (j, i) in solution.keys() if not is_integer_val(solution[(j, i)])]))) <= n_machines, "Too many fractional jobs="+str(list(set([j for (j, i) in solution.keys() if not is_integer_val(solution[(j, i)])])))+" n_machines="+str(n_machines)
+            assert len(list(set([j for (j, i) in solution.keys() if not is_integer_val(solution[(j, i)])]))) <= n_machines, "Too many fractional jobs"
             right = middle
         else:
             left = middle
