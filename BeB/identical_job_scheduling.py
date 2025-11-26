f8f44c201c4b03cfa09e39f56a96fb664b8d5452
ztiziano <tiziano.zamaroni@gmail.com>
2025-11-26 18:41:48 +0100

    new instances and plots

--- BeB/identical_job_scheduling.py
+++ BeB/identical_job_scheduling.py
@@ -206,7 +206,7 @@ class BranchAndBound:
 
         self.verbose = verbose
         self.TOL = 1e-6
-        self.MAX_NODES = 1_000
+        self.MAX_NODES = 2_000
 
         if self.profiling_mode == ProfilingMode.NO_PROFILING:
             self.profiling = None
